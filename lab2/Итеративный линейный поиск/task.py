477625bff5b7b6d31d6d06f3ff5b33b5709201aa
Lapina-Polina <pl295141@gmail.com>
2026-01-07 17:44:38 +0300

    lab2

---
+++ lab2/Итеративный линейный поиск/task.py
@@ -0,0 +1,23 @@
+"""
+This module implements some functions based on linear search algo
+"""
+from typing import List
+
+
+def min_search(arr: List[int]) -> int:
+    """
+    Функция для поиска минимума в массиве
+
+    :param arr: Массив целых чисел
+    :return: Индекс первого вхождения элемента в массиве
+    """
+    if not arr:
+        raise ValueError("Массив не должен быть пустым")
+
+    min_index = 0  # предполагаем, что минимум — первый элемент
+
+    for i in range(1, len(arr)):
+        if arr[i] < arr[min_index]:
+            min_index = i
+
+    return min_index
---
+++ lab2/Итеративный факториал/task.py
@@ -0,0 +1,14 @@
+def factorial_iterative(n: int) -> int:
+    """
+    Рассчитать факториал числа n итеративным способом
+
+    :param n: Число, факториал которого нужно найти
+    :return: n! - факториал числа n
+    """
+    if n < 0:
+        raise ValueError("Факториал определён только для неотрицательных чисел")
+
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
